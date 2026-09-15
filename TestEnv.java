5f4a3def19d3bdc8f7631e81f28a06a46d66d144
shaima-ousji <chaima.elaousji@edu.isetcom.tn>
2026-09-15 21:10:57 +0200

    TPO : Configuration de l'environnement Visual Studio et JDK

---
+++ TestEnv.java
@@ -0,0 +1,7 @@
+public class TestEnv {
+    public static void main(String[] args){
+        System.out.println("=========================================");
+        System.out.println("Environnement visual studio + java pret !");
+        System.out.println("==========================================");
+    }
+}
